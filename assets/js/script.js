4e95a638f810e91eee1c55f704604e07cc2bb7e5
shivamsharma0227 <48487526+shivamsharma0227@users.noreply.github.com>
2019-03-12 23:38:08 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * @author sis119
+ */
+function drop() {
+var btn=document.getElementById('dropdown');
+if(btn.style.display==='block')
+	btn.style.display='none';
+else
+	btn.style.display='block';
+}
